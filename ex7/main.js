70377a2538f9fc5494495d0bf16c75bfb80f2167
Wolfy492 <147376932+Wolfy492@users.noreply.github.com>
2023-10-10 12:56:16 +0200

    ajout ex 7

---
+++ ex7/main.js
@@ -0,0 +1,14 @@
+let names = [];
+//let i = 0
+
+names.push("Vincent", "Paul", "Arthur")
+
+/*names.forEach (element =>  {
+    names[i]  += " va à la pêche"
+    i++*/
+    
+names.forEach(name => {
+    name += " va à la pèche"; //équivalent de name = name + "va à la pèche"
+    console.log(name)
+});
+
